205c59e1ff977b47861a1c67e6d6115f643ae422
David Britton <bubb@Bubbs-Air.hsd1.ca.comcast.net>
2022-03-29 14:47:37 -0700

    Fixed 'random.choice' lack of random-ness. Also finishing up most of the function logic

--- Day_Trip_Generator.py
+++ Day_Trip_Generator.py
@@ -10,25 +10,33 @@ entertainment = ['Movie','Concert','Shopping']
 #functions
 def choose_destination():
     place = random.choice(destinations)
-    print(place)
     destination = input(f'Welcome to the Day trip generator, we have selected {place} for your destination. Would you like to go to {place}? Enter y/n')
     if destination == 'n':
-        print(f"Sorry you don't like that place, how about {place}? ")
+        new_place = random.choice(destinations)
+        print(f"Sorry you don't like that place, how about {new_place}? y/n")
     elif destination == 'y':
             print("Great! Let's move on.")
+    else:
+        print("I'm sorry please type. y/n")
+    
 
 
 def choose_resturant():
     food = random.choice(resturants)
-    print(food)
     resturants = input(f"We have selected {food} as a place to eat. Would you like to eat here? Enter y/n")
+    if resturants == 'n':
+        print(f"Sorry you don't like that resturant. Would you like to go to {food}?")
+    elif resturants == 'y':
+        print("Great! Let's continue")
+
     pass
 
 def choose_transportation():
     moving = random.choice(transportation)
     print(moving)
-    
-    pass
+    transportation = input(f"We have chosen {transportation} as you mode of transportion")
+    if transportation == 'n':
+        print(f"Sorry you don't like that transportation")
 
 def choose_entertainment():
     watching = random.choice(entertainment)
